af4b9e23cf98783fd35fd95d5503a90f8f99bdab
Tseng Kuan Wei <wayne@Tsengde-MacBook-Pro.local>
2019-12-11 13:56:02 +0800

    encode: first update

---
+++ video_writer/encode.py
@@ -0,0 +1,43 @@
+import numpy as np
+import argparse
+import glob
+import cv2
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('imgdir', help='image file path')
+parser.add_argument('-ti' help = 'image file format',default = '.jpg')
+parser.add_argument('-o', help = 'output video directory + name',default = '.')
+parser.add_argument('-f', help = 'output video frame rate', default = 30)
+parser.add_argument('-to',help = 'output video codec', default = '.mp4')
+
+
+args = parser.parse_args()
+imgpath = args.imgdir
+imgarr  = []
+
+# File I/O:
+images = glob.glob(imgpath+'*'+args.ti)
+outname = args.o + args.to
+
+# Codec Specification:
+fourcc = {'.mp4': cv2.VideoWriter_fourcc(*'mp4v'), '.avi' : cv2.VideoWriter_fourcc(*'XVID') }
+
+for fname in images:
+	img = cv2.imread(fname)
+
+	if len(img.shape) == 2:
+		img = cv2.cvtColor(gray,cv2.COLOR_GRAY2BGR)
+
+	imgarr.append(img)
+	h,w,_ = img.shape
+	size = (w,h)
+
+out = cv2.VideoWriter(outname,fourcc, args.f, size)
+
+for i in range(len(imgarr)):
+    out.write(img_array[i])
+
+out.release()
+

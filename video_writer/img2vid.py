dc786e5aae4a960c3cac299fc3c7aefdbbc5a818
Tseng Kuan Wei <wayne@Tsengde-MacBook-Pro.local>
2019-12-11 15:16:07 +0800

    Add smart progress meter

--- video_writer/img2vid.py
+++ video_writer/img2vid.py
@@ -11,33 +11,37 @@ parser.add_argument('-ti',help = 'image file format',default = '.jpg')
 parser.add_argument('-o' ,help = 'output video directory + name',default = 'out')
 parser.add_argument('-f' ,help = 'output video frame rate', default = 30)
 parser.add_argument('-to',help = 'output video codec', default = '.mp4')
-
+parser.add_argument('-v' ,help = 'visualize the process with tqdm.', action ='store_true')
 
 args = parser.parse_args()
 imgpath = args.imgdir
-imgarr  = []
 
 # File I/O:
 images = glob.glob(imgpath+'*'+args.ti)
 outname = args.o + args.to
 
+# Smart Progress Meter:
+if args.v:
+    nof = len(images)
+    from tqdm import tqdm
+    pbar = tqdm(total=nof)
+
 # Codec Specification:
 fourcc = {'.mp4': cv2.VideoWriter_fourcc(*'mp4v'), '.avi' : cv2.VideoWriter_fourcc(*'XVID') }
 
-for fname in images:
-	img = cv2.imread(fname)
-
-	if len(img.shape) == 2:
-		img = cv2.cvtColor(gray,cv2.COLOR_GRAY2BGR)
-
-	imgarr.append(img)
-	h,w,_ = img.shape
-	size = (w,h)
-
+s = cv2.imread(images[0]).shape
+size = (s[1],s[0])
 out = cv2.VideoWriter(outname,fourcc[args.to], args.f, size)
 
-for i in range(len(imgarr)):
-    out.write(imgarr[i])
+for fname in images:
+    img = cv2.imread(fname)
+
+    if len(img.shape) == 2:
+        img = cv2.cvtColor(gray,cv2.COLOR_GRAY2BGR)
+    if args.v:
+        pbar.update(1)	
+
+    out.write(img)
 
 out.release()
 
--- video_writer/vid2img.py
+++ video_writer/vid2img.py
@@ -7,17 +7,29 @@ parser = argparse.ArgumentParser()
 parser.add_argument('viddir',help='video file path')
 parser.add_argument('-to',help = 'output image format (.jpg or .bmp)',default = '.jpg')
 parser.add_argument('-o' ,help = 'output image directory + prefix',default = 'img/img')
+parser.add_argument('-v' ,help = 'visualize the process with tqdm.', action ='store_true')
 
 args = parser.parse_args()
 vid = cv2.VideoCapture(args.viddir)
 cnt = 1
+
+if args.v:
+	nof = int(vid.get(cv2.CAP_PROP_FRAME_COUNT))
+	from tqdm import tqdm
+	pbar = tqdm(total=nof)
 
 while(True):
     ret, img = vid.read()
     if ret:
         cv2.imwrite(args.o + str(cnt) + args.to,img)
         cnt = cnt + 1
+        if args.v:
+        	pbar.update(1)
     else:
         break
-        
+
 vid.release()
+
+if args.v:
+	pbar.close()
+

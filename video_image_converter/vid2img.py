5d210edb8c394e073a7d9a8306631c7357240271
Tseng Kuan Wei <wayne@Tsengde-MacBook-Pro.local>
2019-12-15 15:45:44 +0800

    optimize output path

--- video_image_converter/vid2img.py
+++ video_image_converter/vid2img.py
@@ -1,6 +1,7 @@
 import numpy as np
 import argparse
 import cv2
+import os
 
 parser = argparse.ArgumentParser()
 
@@ -21,7 +22,7 @@ if args.v:
 while(True):
     ret, img = vid.read()
     if ret:
-        cv2.imwrite(args.o + str(cnt) + args.to,img)
+        cv2.imwrite(os.path.join(args.o,str(cnt),args.to,img))
         cnt = cnt + 1
         if args.v:
         	pbar.update(1)
